1b18c95d23ecfbb5ed39db14137c25329d6649de
Estevao <estevo144@gmail.com>
2022-08-24 11:32:41 -0300

    subindo a 4 +- feita, vou mexer com ela fora de casa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import Form from './components/Form';
-import Card from './components/Card';
+import React from "react";
+import Form from "./components/Form";
+import Card from "./components/Card";
+import { func } from "prop-types";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: "",
+      cardAttr2: "",
+      cardAttr3: "",
+      cardImage: "",
+      cardRare: "",
+      cardTrunfo: "",
+      isSaveButtonDisabled: false,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    console.log(target);
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value }, () => {
+        const {
+          cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardImage,
+        cardRare,
+      } = this.state;
+
+        }
+      }
+    )
+  };
+
   render() {
     return (
       <div>
