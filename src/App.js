2a7f970729a4021edba4165163adfb4b5fc9e285
Estevao <estevo144@gmail.com>
2022-08-25 17:24:46 -0300

    questao 5 feita com lint

--- src/App.js
+++ src/App.js
@@ -12,13 +12,50 @@ class App extends React.Component {
     cardImage: '',
     cardRare: '',
     cardTrunfo: '',
-    isSaveButtonDisabled: false,
+    isSaveButtonDisabled: true,
+  };
+
+  numMax = (numero) => {
+    const valorMax = 90;
+    if (Number(numero) >= 0 && Number(numero) <= valorMax) {
+      return true;
+    }
   };
 
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => {
+      const {
+        cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardImage,
+        cardRare,
+      } = this.state;
+
+      const attr1 = this.numMax(cardAttr1);
+      const attr2 = this.numMax(cardAttr2);
+      const attr3 = this.numMax(cardAttr3);
+      const soma = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+      const somaMax = 210;
+      if (
+        cardName
+        && cardDescription
+        && cardImage
+        && cardRare
+        && soma <= somaMax
+        && attr1
+        && attr2
+        && attr3
+      ) {
+        this.setState({ isSaveButtonDisabled: false });
+      } else {
+        this.setState({ isSaveButtonDisabled: true });
+      }
+    });
   };
 
   render() {
